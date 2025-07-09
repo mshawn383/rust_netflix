41e975d076ac8f2001f6459fd748436a36b5b832
mshawn383 <mshawn383@gmail.com>
2025-07-10 04:23:33 +0530

    Update main.rs

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -2,16 +2,46 @@ use leptos::*;
 use leptos::prelude::*;
 
 
-fn main() {
-    mount_to_body(move || {
-        view! {
-            <video 
-            controls
-           id="video"
-           width="640"
-           height="360"
-            />
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = window)]
+    fn Hls() -> JsValue;
+}
+
+#[component]
+fn App() -> impl IntoView {
+    leptos::on_mount(|| {
+        let hls = js_sys::Reflect::get(&window().unwrap(), &"Hls".into()).unwrap();
+        let is_supported = js_sys::Reflect::get(&hls, &"isSupported".into()).unwrap()
+            .dyn_into::<js_sys::Function>().unwrap()
+            .call0(&JsValue::NULL).unwrap()
+            .as_bool().unwrap();
+
+        if is_supported {
+            let video = window().unwrap()
+                .document().unwrap()
+                .get_element_by_id("video").unwrap()
+                .dyn_into::<HtmlVideoElement>().unwrap();
+
+            let hls_instance = js_sys::Reflect::construct(&hls, &js_sys::Array::new()).unwrap();
+            let load_source = js_sys::Reflect::get(&hls_instance, &"loadSource".into()).unwrap()
+                .dyn_into::<js_sys::Function>().unwrap();
+            load_source.call1(&hls_instance, &JsValue::from_str("output.m3u8")).unwrap();
+
+            let attach_media = js_sys::Reflect::get(&hls_instance, &"attachMedia".into()).unwrap()
+                .dyn_into::<js_sys::Function>().unwrap();
+            attach_media.call1(&hls_instance, &video.into()).unwrap();
         }
     });
+
+    view! {
+        <video id="video" controls width="640" height="360" />
+    }
+}
+
+
+fn main() {
+    mount_to_body(App);
 
 }
